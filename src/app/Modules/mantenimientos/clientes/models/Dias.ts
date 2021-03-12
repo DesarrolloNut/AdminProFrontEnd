a976173156c4815900f04470e29cad7c7048703b
Javier Ismael De Jesus Martinez <ismael.dt@hotmail.com>
2021-03-12 16:55:21 -0400

    Frecuencia de Visita: Listo

---
+++ src/app/Modules/mantenimientos/clientes/models/Dias.ts
@@ -0,0 +1,14 @@
+export class Dias {
+
+    constructor() {
+        this.id = 0;
+        this.dia = 0;
+        this.nombre = "";
+
+    }
+
+    id: number
+    dia: number
+    nombre: string
+
+}
