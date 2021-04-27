f9b9ffeab1120bd5d4de410798d2d39319f5a69e
Jancer Fabian Rodriguez <jancer.fabian@gmail.com>
2021-04-27 17:03:54 -0400

    Formulario de cotizaciones inicio

--- src/app/Modules/ventas/cotizaciones/cotizaciones-formulario/cotizaciones-formulario.component.ts
+++ src/app/Modules/ventas/cotizaciones/cotizaciones-formulario/cotizaciones-formulario.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthenticationService } from 'src/app/core/authentication/service/authentication.service';
+import { Parametro } from 'src/app/core/http/model/Parametro';
+import { BackendService } from 'src/app/core/http/service/backend.service';
+import { Almacen } from 'src/app/Modules/mantenimientos/almacenes/models/Almacen';
+import { DataApi } from 'src/app/shared/enums/DataApi.enum';
+import { ComboBox } from 'src/app/shared/model/ComboBox';
 
 @Component({
   selector: 'app-cotizaciones-formulario',
@@ -7,9 +16,139 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CotizacionesFormularioComponent implements OnInit {
 
-  constructor() { }
+  companias: ComboBox[] = [];
+
+  loadingCompanias = false;
+
+  Cargando: boolean = false;
+  Formulario: FormGroup;
+  submitted = false;
+  btnGuardarCargando = false;
+  actualizando = false;
+  loadingClientes: boolean;
+  clientes: ComboBox[];
+
+  constructor(
+    private toastService: ToastrService,
+    private route: ActivatedRoute,
+    private httpService: BackendService,
+    private router: Router,
+    private authService: AuthenticationService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
+    // let id = Number(this.route.snapshot.paramMap.get('id'));
+
+    // if (id > 0) {
+    //   this.getItem(id);
+    //   this.actualizando = true;
+    // }
+
+    this.CreateForm();
+    // this.getClientes()
   }
+
+
+  private CreateForm() {
+
+    this.Formulario = this.formBuilder.group({
+      id: [0],
+      nombre: [null, [Validators.required]],
+      companiaID: [null, Validators.required],
+      clienteID: [null, Validators.required],
+      codigoReferencia: [null, Validators.required],
+      descripcion: [null,],
+      estadoID: [0,],
+    });
+  }
+
+  get f() { return this.Formulario.controls; } // acceder a los controles del formulario para no escribir tanto codigo en el html
+
+  getItem(id: number) {
+    this.Cargando = true;
+    this.httpService.DoPostAny<Almacen>(DataApi.Almacen,
+      "GetAlmacenByID", id).subscribe(response => {
+        if (!response.ok) {
+          this.toastService.error(response.errores[0]);
+        } else {
+          //validar que existe
+          if (response != null && response.records != null && response.records.length > 0) {
+            let record = response.records[0]
+            this.Formulario.setValue(record);
+          } else {
+            this.toastService.warning("Almacen no encontrado");
+            this.router.navigateByUrl('/mantenimientos/almacen');
+          }
+        }
+
+      }, error => {
+        this.Cargando = false;
+        this.toastService.error("Error conexion al servidor");
+      });
+  }
+
+
+  onSubmit() {
+
+    this.submitted = true;
+    if (this.Formulario.invalid) {
+      return;
+    }
+    this.guardar();
+  }
+
+
+  guardar() {
+
+    let metodo: string = this.actualizando ? "Update" : "Registrar";
+    this.btnGuardarCargando = true;
+
+    this.httpService.DoPostAny<Almacen>(DataApi.Almacen,
+      metodo, this.Formulario.value).subscribe(response => {
+
+        if (!response.ok) {
+          this.toastService.error(response.errores[0], "Error");
+        } else {
+          this.toastService.success("Realizado", "OK");
+          this.router.navigateByUrl('/mantenimientos/almacen');
+        }
+
+        this.btnGuardarCargando = false;
+      }, error => {
+        this.btnGuardarCargando = false;
+        this.toastService.error("Error conexion al servidor");
+      });
+  }
+
+  getClientes(searchObj: any = null, clienteID: number = 0) {
+    let search = ""
+
+    if (searchObj)
+      search = searchObj.term;
+
+    this.loadingClientes = true;
+    let parametros: Parametro[] = [
+      { key: "CompaniaID", value: this.authService.tokenDecoded.primarygroupsid },
+      { key: "Search", value: search },
+      { key: "clienteID", value: clienteID },
+    ];
+    this.httpService.DoPost<ComboBox>(DataApi.ComboBox,
+      "GetClientesComboBox", parametros).subscribe(response => {
+
+        if (!response.ok) {
+          this.toastService.error(response.errores[0]);
+        } else {
+          this.clientes = response.records;
+        }
+
+        this.loadingClientes = false;
+      }, error => {
+        this.loadingClientes = false;
+        this.toastService.error("Error conexion al servidor");
+      });
+  }
+
+
 
 }
