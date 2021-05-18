e523ee20819a8602f7370dc4dcd494626598512b
Jancer Fabian Rodriguez <jancer.fabian@gmail.com>
2021-05-18 17:08:45 -0400

    importar shared module en modulo de compras

--- src/app/Modules/compras/compras.module.ts
+++ src/app/Modules/compras/compras.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -11,6 +12,7 @@ import { SolicitudComprasFormularioComponent } from './solicitudCompras/solicitu
   declarations: [ComprasComponent, SolicitudComprasListadoComponent, SolicitudComprasFormularioComponent],
   imports: [
     CommonModule,
+    SharedModule,
     ComprasRoutingModule
   ]
 })
