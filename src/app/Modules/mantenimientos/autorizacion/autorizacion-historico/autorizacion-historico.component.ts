d264fed4233246aabf2a2bce6d3ab5fb1238cecd
Jancer Fabian Rodriguez <jancer.fabian@gmail.com>
2021-07-14 17:24:07 -0400

    autorizacion-historico | haciendo el componente

--- src/app/Modules/mantenimientos/autorizacion/autorizacion-historico/autorizacion-historico.component.ts
+++ src/app/Modules/mantenimientos/autorizacion/autorizacion-historico/autorizacion-historico.component.ts
@@ -7,9 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutorizacionHistoricoComponent implements OnInit {
 
-  constructor() { }
+  // COPIAR AL CREAR UN LISTADO NUEVO
+  Search: string = "";
+  paginaNumeroActual = 1;
+  Cargando: boolean = false;
+  CargandoBar: boolean = false;
+  totalPaginas: number = 0;
+  paginaSize: number = 5;
+  paginaTotalRecords: number = 0;
+  data: AlmacenListadoViewModel[] = [] //tu modelo
+
+  constructor(private toastService: ToastrService,
+    private httpService: BackendService,
+    public permissionsService: NgxPermissionsService,
+  ) { }
+
 
   ngOnInit(): void {
+    this.getData()
+  }
+  getData() {
+    this.Cargando = true;
+
+    let parametros: Parametro[] = [{ key: "Search", value: this.Search }]
+
+    this.httpService.GetAllWithPagination<AlmacenListadoViewModel>(DataApi.Almacen, "GetAlmacenListado", "ID", this.paginaNumeroActual,
+      this.paginaSize, true, parametros).subscribe(x => {
+
+        if (x.ok) {
+          this.data = x.records;
+          this.asignarPagination(x);
+        } else {
+          this.toastService.error(x.errores[0]);
+          console.error(x.errores[0]);
+        }
+        this.Cargando = false;
+      }, error => {
+        console.error(error);
+        this.toastService.error("Error conexion al servidor");
+        this.Cargando = false;
+      });
+
   }
 
+
+  asignarPagination(x: ResponseContenido<any>) {
+
+    if (x.pagina != null) {
+      this.totalPaginas = x.pagina.totalPaginas == null ? 0 : x.pagina.totalPaginas;
+      this.paginaTotalRecords = x.pagina.totalRecords == null ? 0 : x.pagina.totalRecords;
+      this.paginaSize = x.pagina.paginaSize == null ? 0 : x.pagina.paginaSize;
+    } else {
+      this.totalPaginas = 0;
+      this.paginaTotalRecords = 0;
+      this.paginaSize = 0;
+    }
+
+  }
 }
