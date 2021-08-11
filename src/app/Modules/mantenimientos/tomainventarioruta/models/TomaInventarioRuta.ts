dc76a535deb7a3c730ccf070f5c3b8bfea79e05e
Javier Ismael De Jesus Martinez <ismael.dt@hotmail.com>
2021-08-11 16:38:14 -0400

    cambios en toma inventario

--- src/app/Modules/mantenimientos/tomainventarioruta/models/TomaInventarioRuta.ts
+++ src/app/Modules/mantenimientos/tomainventarioruta/models/TomaInventarioRuta.ts
@@ -1,44 +1,58 @@
+
 export class TomaInventarioRuta {
-  constructor() {
-    this.clienteId = 0;
-    this.cliente = '';
-    this.direccion = '';
-    this.cantidad = 0;
-    this.disponible = 0;
-    this.fecha = '';
-    this.merchandising = '';
-    this.vendedor = '';
-    this.rutaRecogidaId = 0;
-    this.usuarioId = 0;
-    this.recogedor = '';
-    this.diaId = 0;
-    this.ordenVisita = 0;
-    this.zona = '';
-    this.editarUsuarioConfirmado = true;
-    this.cancelarUsuarioConfirmado = false;
-    this.cambioUsuarioConfirmado = false;
-  }
 
-  clienteId: number;
-  cliente: string;
-  direccion: string;
-  cantidad: number;
-  disponible: number;
-  fecha: string;
-  merchandising: string;
-  vendedor: string;
-  rutaRecogidaId: number;
-  usuarioId: number;
-  recogedor: string;
-  diaId: number;
-  ordenVisita: number;
-  zona: string;
-  editarUsuarioConfirmado: boolean;
-  cancelarUsuarioConfirmado: boolean;
-  cambioUsuarioConfirmado: boolean;
-
+constructor(){
+  this.id = 0;
+  this.usuarioId = 0;
+  this.usuario = '';
+  this.celular = '';
+  this.cantidadCliente = 0;
+  this.clienteId = 0;
+  this.cliente = '';
+  this.documento = '';
+  this.direccion = '';
+  this.provinciaId = 0;
+  this.sectorId = 0;
+  this.articuloId = 0;
+  this.cantidad = '';
+  this.disponible = '';
+  this.fecha = '';
+  this.merchandising = '';
+  this.ruta_Merchandising = 0;
+  this.vendedor = '';
+  this.rutaRecogidaId = 0;
+  this.recogedor = '';
+  this.zonaId = '';
+  this.zona = '';
+  this.rutaId = 0;
+  this.diaId = 0;
 }
 
+  id: number;
+  usuarioId: number;
+  usuario: string;
+  celular: string;
+  cantidadCliente: number;
+  clienteId: number;
+  cliente: string;
+  documento: string;
+  direccion: string;
+  provinciaId: number;
+  sectorId: number;
+  articuloId: number;
+  cantidad: string;
+  disponible: string;
+  fecha: string;
+  merchandising: string;
+  ruta_Merchandising: number;
+  vendedor: string;
+  rutaRecogidaId: number;
+  recogedor: string;
+  zonaId: string;
+  zona: string;
+  rutaId: number;
+  diaId: number;
+}
 
 
 
--- src/app/Modules/mantenimientos/tomainventarioruta/tomainventarioruta-listado/tomainventarioruta-listado.component.ts
+++ src/app/Modules/mantenimientos/tomainventarioruta/tomainventarioruta-listado/tomainventarioruta-listado.component.ts
@@ -6,9 +6,10 @@ import { Parametro } from 'src/app/core/http/model/Parametro';
 import { ResponseContenido } from 'src/app/core/http/model/ResponseContenido';
 import { BackendService } from 'src/app/core/http/service/backend.service';
 import { DataApi } from 'src/app/shared/enums/DataApi.enum';
-import { RollUsuario } from 'src/app/shared/enums/RollUsuario';
 import { ComboBox } from 'src/app/shared/model/ComboBox';
 import { TomaInventarioRuta } from '../models/TomaInventarioRuta';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DualListComponent } from 'angular-dual-listbox';
 
 @Component({
   selector: 'app-tomainventario-listado',
@@ -16,51 +17,86 @@ import { TomaInventarioRuta } from '../models/TomaInventarioRuta';
   styleUrls: ['./tomainventarioruta-listado.component.scss']
 })
 export class TomainventarioRutaListadoComponent implements OnInit {
-// COPIAR AL CREAR UN LISTADO NUEVO
-Search: string = "";
-paginaNumeroActual = 1;
-Cargando: boolean = false;
-CargandoBar: boolean = false;
-totalPaginas: number = 0;
-paginaSize: number = 5;
-paginaTotalRecords: number = 0;
-data: TomaInventarioRuta[] = [] //tu modelo
-btnGuardarCargando: boolean = false;
-loadingUsuarios: boolean;
-loadingFrecuenciaVisita: boolean;
-Usuarios: ComboBox[];
-Dias: ComboBox[] = [
-  {codigo: 1, nombre: 'Lunes', grupo:'', grupoID:''},
-  {codigo: 2, nombre: 'Martes', grupo:'', grupoID:''},
-  {codigo: 3, nombre: 'Miercoles', grupo:'', grupoID:''},
-  {codigo: 4, nombre: 'Jueves', grupo:'', grupoID:''},
-  {codigo: 5, nombre: 'Viernes', grupo:'', grupoID:''},
-  {codigo: 6, nombre: 'Sabado', grupo:'', grupoID:''},
-  {codigo: 7, nombre: 'Domingo', grupo:'', grupoID:''},
-];
-FrecuenciaVisita: FrecuenciaVisita[];
-constructor(private toastService: ToastrService,
-  private httpService: BackendService,
-  public permissionsService: NgxPermissionsService,
-) { }
+  // COPIAR AL CREAR UN LISTADO NUEVO
+  Search: string = "";
+  paginaNumeroActual = 1;
+  Cargando: boolean = false;
+  CargandoBar: boolean = false;
+  totalPaginas: number = 0;
+  paginaSize: number = 5;
+  paginaTotalRecords: number = 0;
+  dataListado: TomaInventarioRuta[] = [] //tu modelo
+  selectData: TomaInventarioRuta = new TomaInventarioRuta(); //tu modelo
+  btnGuardarCargando: boolean = false;
+  loadingTipoRutas: boolean;
+  loadingFrecuenciaVisita: boolean;
+
+  Dias: ComboBox[] = [
+    { codigo: 1, nombre: 'Lunes', grupo: '', grupoID: '' },
+    { codigo: 2, nombre: 'Martes', grupo: '', grupoID: '' },
+    { codigo: 3, nombre: 'Miercoles', grupo: '', grupoID: '' },
+    { codigo: 4, nombre: 'Jueves', grupo: '', grupoID: '' },
+    { codigo: 5, nombre: 'Viernes', grupo: '', grupoID: '' },
+    { codigo: 6, nombre: 'Sabado', grupo: '', grupoID: '' },
+    { codigo: 7, nombre: 'Domingo', grupo: '', grupoID: '' },
+  ];
+  FrecuenciaVisita: FrecuenciaVisita[];
+  TipoRutas: ComboBox[];
+  TipoRutaId: number = 1;
+
+  // Dual List options
+  tab = 1;
+  keepSorted = true;
+  key: string;
+  display: string;
+  filter = true;
+  source: Array<TomaInventarioRuta> = [];
+  confirmed: Array<TomaInventarioRuta> = [];
+  userAdd = '';
+  disabled = false;
+  sourceLeft = true;
+  // format: any = DualListComponent.DEFAULT_FORMAT;
+  format = {
+    add: 'Agregar', remove: 'Remover', all: 'Seleccionar Todos', none: 'Deseleccionar',
+    direction: DualListComponent.LTR, draggable: true, locale: 'da'
+  };
+  loadingDualLits: boolean;
+  loadingClientesTomaInventario: boolean;
+  IsRecogida: boolean = false;
 
 
-ngOnInit(): void {
-  this.getData();
-  this.getUsuariobyRolID();
-}
-getData() {
-  this.Cargando = true;
+  constructor(private toastService: ToastrService,
+    private httpService: BackendService,
+    private modalService: NgbModal,
+    public permissionsService: NgxPermissionsService,
+  ) { }
 
-  let parametros: Parametro[] = [{ key: "Search", value: this.Search }]
 
-  this.httpService.GetAllWithPagination<TomaInventarioRuta>(DataApi.TomaInventarioRuta, "GetTomaInventarioRutaListado", "zona", this.paginaNumeroActual,
+  ngOnInit(): void {
+    this.getData();
+    this.getTipoRutas();
+    this.configDualList();
+  }
 
-    this.paginaSize, true, parametros).subscribe(x => {
+  configDualList() {
+    this.key = 'id';
+    this.display = 'cliente';
+    this.keepSorted = true;
+  }
+
+  getData() {
+    this.Cargando = true;
+
+    let parametros: Parametro[] = [
+      { key: "Search", value: this.Search },
+      { key: "tipoRutaId", value: this.TipoRutaId }
+    ]
+
+    this.httpService.GetAllWithPagination<TomaInventarioRuta>(DataApi.TomaInventarioRuta, "GetListadoUsuarioRutas", "UsuarioId", this.paginaNumeroActual, this.paginaSize, true, parametros).subscribe(x => {
 
       if (x.ok) {
-        x.records.forEach(x => x.editarUsuarioConfirmado = true);
-        this.data = x.records;
+        //x.records.forEach(x => x.editarUsuarioConfirmado = true);
+        this.dataListado = x.records;
         // console.log(x);
         this.asignarPagination(x);
       } else {
@@ -74,113 +110,264 @@ getData() {
       this.Cargando = false;
     });
 
-}
-
-
-
-onChangeEdit(value:TomaInventarioRuta){
-  value.editarUsuarioConfirmado = !value.editarUsuarioConfirmado;
-  value.cancelarUsuarioConfirmado = !value.cancelarUsuarioConfirmado;
-  value.cambioUsuarioConfirmado = value.editarUsuarioConfirmado ? false : true;
-}
-
-
-SaveChanges(value:TomaInventarioRuta) {
-
-
-  this.btnGuardarCargando = true;
-  this.httpService.DoPostAny<any>(DataApi.TomaInventarioRuta, "Update", value).subscribe(x => {
-
-      if (x.ok) {
-        this.getData();
-      } else {
-        this.toastService.error(x.errores[0]);
-        console.error(x.errores[0]);
-      }
-      this.btnGuardarCargando = false;
-    }, error => {
-      console.error(error);
-      this.toastService.error("Error conexion al servidor");
-      this.btnGuardarCargando = false;
-    });
-
-  this.httpService.DoPostAny<any>(DataApi.TomaInventarioRuta, "InsertOrUpdateFrecuenciaVisita", value).subscribe(x => {
-
-      if (x.ok) {
-        this.getData();
-      } else {
-        this.toastService.error(x.errores[0]);
-        console.error(x.errores[0]);
-      }
-    }, error => {
-      console.error(error);
-      this.toastService.error("Error conexion al servidor");
-    });
-    this.onChangeEdit(value);
-
-
-
-
-
-}
-
-getUsuariobyRolID() {
-  this.loadingUsuarios = true;
-  let parametro: Parametro[] = [{ key: "RollID", value: RollUsuario.RECOGEDOR }];
-  this.httpService.DoPost<ComboBox>(DataApi.ComboBox,
-    "GetUsuarioPorRollComboBox", parametro).subscribe(response => {
-
-      if (!response.ok) {
-        this.toastService.error(response.errores[0]);
-      } else {
-        this.Usuarios = response.records;
-      }
-      this.loadingUsuarios = false;
-    }, error => {
-      this.loadingUsuarios = false;
-      this.toastService.error("No se pudo obtener los supervisores", "Error conexion al servidor");
-
-      setTimeout(() => {
-        this.getUsuariobyRolID()
-      }, 1000);
-
-    });
-}
-
-getFrecuenciaVisita(value:TomaInventarioRuta) {
-  this.loadingFrecuenciaVisita = true;
-  this.httpService.DoPostAny<FrecuenciaVisita>(DataApi.TomaInventarioRuta,
-    "getClienteFrecuenciaVisitaEnrrollByID", value.clienteId).subscribe(response => {
-
-      if (!response.ok) {
-        this.toastService.error(response.errores[0]);
-      } else {
-        this.FrecuenciaVisita = response.records;
-      }
-      this.loadingFrecuenciaVisita = false;
-    }, error => {
-      this.loadingFrecuenciaVisita = false;
-      this.toastService.error("No se pudo obtener los supervisores", "Error conexion al servidor");
-
-      setTimeout(() => {
-        this.getUsuariobyRolID()
-      }, 1000);
-
-    });
-}
-
-asignarPagination(x: ResponseContenido<any>) {
-
-  if (x.pagina != null) {
-    this.totalPaginas = x.pagina.totalPaginas == null ? 0 : x.pagina.totalPaginas;
-    this.paginaTotalRecords = x.pagina.totalRecords == null ? 0 : x.pagina.totalRecords;
-    this.paginaSize = x.pagina.paginaSize == null ? 0 : x.pagina.paginaSize;
-  } else {
-    this.totalPaginas = 0;
-    this.paginaTotalRecords = 0;
-    this.paginaSize = 0;
   }
 
-}
+
+  getClientebyRutaTipoId() {
+    this.loadingDualLits = true;
+
+    this.httpService.DoPostAny<TomaInventarioRuta>(DataApi.TomaInventarioRuta, "GetClientesByTipoRutaId", this.TipoRutaId).subscribe(x => {
+
+      if (x.ok) {
+        this.source = x.records;
+        // console.log(this.source);
+      } else {
+        this.toastService.error(x.errores[0]);
+        console.error(x.errores[0]);
+      }
+      this.loadingDualLits = false;
+    }, error => {
+      console.error(error);
+      this.toastService.error("Error conexion al servidor");
+      this.loadingDualLits = false;
+    });
+
+  }
+
+  getClientesTomaInventarioPendiente() {
+    this.loadingDualLits = true;
+
+    this.httpService.DoPostAny<TomaInventarioRuta>(DataApi.TomaInventarioRuta, "GetClientesTomaInventarioPendiente", {}).subscribe(x => {
+
+      if (x.ok) {
+        this.source = [];
+        let data = x.records;
+        for (let index = 0; index < data.length; index++) {
+          data[index].id = index + 1;
+          this.source.push(data[index]);
+        }
+        // console.log("source",x.records);
+      } else {
+        this.toastService.error(x.errores[0]);
+        console.error(x.errores[0]);
+      }
+      this.loadingDualLits = false;
+    }, error => {
+      console.error(error);
+      this.toastService.error("Error conexion al servidor");
+      this.loadingDualLits = false;
+    });
+
+  }
+
+
+  getClientesTomaInventarioSelecionados(model: TomaInventarioRuta) {
+    this.loadingDualLits = true;
+    this.httpService.DoPostAny<TomaInventarioRuta>(DataApi.TomaInventarioRuta, "GetClientesTomaInventarioSelecionados", model).subscribe(x => {
+
+      if (x.ok) {
+        this.confirmed = [];
+        let data = x.records;
+        for (let index = 0; index < data.length; index++) {
+          data[index].id = index + 1;
+          this.confirmed.push(data[index]);
+        }
+        this.selectData = x.records[0];
+      } else {
+        this.toastService.error(x.errores[0]);
+        console.error(x.errores[0]);
+      }
+      this.loadingDualLits = false;
+    }, error => {
+      console.error(error);
+      this.toastService.error("Error conexion al servidor");
+      this.loadingDualLits = false;
+    });
+
+  }
+
+  openModal(content, model: TomaInventarioRuta) {
+
+
+    if (this.TipoRutaId == 3) {
+      this.IsRecogida = true;
+      this.getClientesTomaInventarioPendiente();
+      this.getClientesTomaInventarioSelecionados(model);
+
+    } else {
+      this.IsRecogida = true;
+      this.getClientebyRutaTipoId();
+    }
+    this.modalService.open(content, { size: 'xl', backdrop: "static", });
+  }
+
+
+
+
+
+
+  SaveChanges() {
+
+    console.log(this.source)
+    console.log(this.confirmed)
+    console.log(this.selectData)
+    // this.btnGuardarCargando = true;
+    // this.httpService.DoPostAny<any>(DataApi.TomaInventarioRuta, "Update", null).subscribe(x => {
+
+    //     if (x.ok) {
+    //       this.getData();
+    //     } else {
+    //       this.toastService.error(x.errores[0]);
+    //       console.error(x.errores[0]);
+    //     }
+    //     this.btnGuardarCargando = false;
+    //   }, error => {
+    //     console.error(error);
+    //     this.toastService.error("Error conexion al servidor");
+    //     this.btnGuardarCargando = false;
+    //   });
+
+    // this.httpService.DoPostAny<any>(DataApi.TomaInventarioRuta, "InsertOrUpdateFrecuenciaVisita", null).subscribe(x => {
+
+    //     if (x.ok) {
+    //       this.getData();
+    //     } else {
+    //       this.toastService.error(x.errores[0]);
+    //       console.error(x.errores[0]);
+    //     }
+    //   }, error => {
+    //     console.error(error);
+    //     this.toastService.error("Error conexion al servidor");
+    //   });
+
+
+
+
+
+  }
+
+  getTipoRutas() {
+    this.loadingTipoRutas = true;
+    this.httpService.DoPost<ComboBox>(DataApi.ComboBox,
+      "GetRutaTipoComboBox", null).subscribe(response => {
+
+        if (!response.ok) {
+          this.toastService.error(response.errores[0]);
+        } else {
+          this.TipoRutas = response.records;
+        }
+        this.loadingTipoRutas = false;
+      }, error => {
+        this.loadingTipoRutas = false;
+        this.toastService.error("No se pudo obtener los supervisores", "Error conexion al servidor");
+
+        setTimeout(() => {
+          this.getTipoRutas()
+        }, 1000);
+
+      });
+  }
+
+  getProvincia() {
+    this.loadingTipoRutas = true;
+    this.httpService.DoPost<ComboBox>(DataApi.ComboBox,
+      "GetProvincias", null).subscribe(response => {
+
+        if (!response.ok) {
+          this.toastService.error(response.errores[0]);
+        } else {
+          this.TipoRutas = response.records;
+        }
+        this.loadingTipoRutas = false;
+      }, error => {
+        this.loadingTipoRutas = false;
+        this.toastService.error("No se pudo obtener los supervisores", "Error conexion al servidor");
+
+        setTimeout(() => {
+          this.getTipoRutas()
+        }, 1000);
+
+      });
+  }
+
+  getSector() {
+    this.loadingTipoRutas = true;
+    this.httpService.DoPost<ComboBox>(DataApi.ComboBox,
+      "GetRutaTipoComboBox", null).subscribe(response => {
+
+        if (!response.ok) {
+          this.toastService.error(response.errores[0]);
+        } else {
+          this.TipoRutas = response.records;
+        }
+        this.loadingTipoRutas = false;
+      }, error => {
+        this.loadingTipoRutas = false;
+        this.toastService.error("No se pudo obtener los supervisores", "Error conexion al servidor");
+
+        setTimeout(() => {
+          this.getTipoRutas()
+        }, 1000);
+
+      });
+  }
+
+  getZonas() {
+    this.loadingTipoRutas = true;
+    this.httpService.DoPost<ComboBox>(DataApi.ComboBox,
+      "GetRutaTipoComboBox", null).subscribe(response => {
+
+        if (!response.ok) {
+          this.toastService.error(response.errores[0]);
+        } else {
+          this.TipoRutas = response.records;
+        }
+        this.loadingTipoRutas = false;
+      }, error => {
+        this.loadingTipoRutas = false;
+        this.toastService.error("No se pudo obtener los supervisores", "Error conexion al servidor");
+
+        setTimeout(() => {
+          this.getTipoRutas()
+        }, 1000);
+
+      });
+  }
+
+  getFrecuenciaVisita(value: TomaInventarioRuta) {
+    this.loadingFrecuenciaVisita = true;
+    this.httpService.DoPostAny<FrecuenciaVisita>(DataApi.TomaInventarioRuta,
+      "getClienteFrecuenciaVisitaEnrrollByID", value.usuarioId).subscribe(response => {
+
+        if (!response.ok) {
+          this.toastService.error(response.errores[0]);
+        } else {
+          this.FrecuenciaVisita = response.records;
+        }
+        this.loadingFrecuenciaVisita = false;
+      }, error => {
+        this.loadingFrecuenciaVisita = false;
+        this.toastService.error("No se pudo obtener los supervisores", "Error conexion al servidor");
+
+        setTimeout(() => {
+          //this.getUsuariobyRolID()
+        }, 1000);
+
+      });
+  }
+
+  asignarPagination(x: ResponseContenido<any>) {
+
+    if (x.pagina != null) {
+      this.totalPaginas = x.pagina.totalPaginas == null ? 0 : x.pagina.totalPaginas;
+      this.paginaTotalRecords = x.pagina.totalRecords == null ? 0 : x.pagina.totalRecords;
+      this.paginaSize = x.pagina.paginaSize == null ? 0 : x.pagina.paginaSize;
+    } else {
+      this.totalPaginas = 0;
+      this.paginaTotalRecords = 0;
+      this.paginaSize = 0;
+    }
+
+  }
 
 }
