7acec33ac17b0d5289815d51be36a7518a516677
Jancer Fabian <jancer.fabian@gmail.com>
2021-09-15 11:43:03 -0400

    Update pesaje-listado.component.ts

--- src/app/Modules/produccion/pesaje/pesaje-listado/pesaje-listado.component.ts
+++ src/app/Modules/produccion/pesaje/pesaje-listado/pesaje-listado.component.ts
@@ -196,9 +196,16 @@ export class PesajeListadoComponent implements OnInit, OnDestroy {
           this.modalService.dismissAll();
         } else {
           if (response.records.length > 0) {
-            this.articuloPesaje = response.records[0];
 
-            this.articuloPesajeDetalle = JSON.parse(this.articuloPesaje.detalleJSON);
+            if (this.articuloPesaje.almacenHastaID == this.almacenDefault) {
+              this.articuloPesaje = response.records[0];
+              this.articuloPesajeDetalle = JSON.parse(this.articuloPesaje.detalleJSON);
+            } else {
+              this.toastService.error("Pesaje encontrado, pero no pertenece a este almacen.")
+              this.modalService.dismissAll()
+            }
+
+
           } else {
             this.toastService.error("Resultado no encontrado")
             this.modalService.dismissAll();
