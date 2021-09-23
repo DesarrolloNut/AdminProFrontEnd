7181091cb7c1a8b5eccef3d4df9a39be425e5f19
LonyStewart142 <lonymejia142@gmail.com>
2021-09-23 16:33:58 -0400

    resolviendo bugs en cliente y avances en despacho

--- src/app/Modules/mantenimientos/recall/recall-listado/recall-listado.component.ts
+++ src/app/Modules/mantenimientos/recall/recall-listado/recall-listado.component.ts
@@ -31,7 +31,7 @@ export class RecallListadoComponent implements OnInit {
 
   recallID: number;
   guardandoDataExcel: boolean;
-
+ 
 
   constructor(private toastService: ToastrService,
     private httpService: BackendService,
