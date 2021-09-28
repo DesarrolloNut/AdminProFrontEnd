cf5e9c2511303457a8bfab51399d3dc02774c4bd
LonyStewart142 <lonymejia142@gmail.com>
2021-09-28 18:44:40 -0400

    despacho in progress

--- src/app/Modules/inventario/despacho/despacho-listado/despacho-listado.component.ts
+++ src/app/Modules/inventario/despacho/despacho-listado/despacho-listado.component.ts
@@ -8,6 +8,8 @@ import { Parametro } from 'src/app/core/http/model/Parametro';
 import { ResponseContenido } from 'src/app/core/http/model/ResponseContenido';
 import { BackendService } from 'src/app/core/http/service/backend.service';
 import { Cliente } from 'src/app/Modules/mantenimientos/clientes/models/Cliente';
+import { ArticuloPesosExtras } from 'src/app/Modules/produccion/pesaje/models/ArticuloPesosExtras';
+import { ArticuloPesosExtrasViewModel } from 'src/app/Modules/produccion/pesaje/models/ArticuloPesosExtrasViewModel';
 import { DataApi } from 'src/app/shared/enums/DataApi.enum';
 import { ComboBox } from 'src/app/shared/model/ComboBox';
 import { DespachoPedidoDetalleViewModel } from '../models/DespachoPedidoDetalleViewModel';
@@ -44,6 +46,16 @@ export class DespachoListadoComponent implements OnInit {
   clienteExiste=true;
   cliente: Cliente;
 
+  canales:ComboBox[]=[];
+  canal:number=0;
+
+
+
+// MOVER A OTRO COMPONENTE
+loadingArticulosExtras: boolean;
+articulosExtrasComboBox: ArticuloPesosExtras[];
+articulosExtras: ArticuloPesosExtrasViewModel[];
+
   constructor(private toastService: ToastrService,
     private httpService: BackendService,
     private modalService: NgbModal,
@@ -55,19 +67,26 @@ export class DespachoListadoComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.getEstados()
-    this.getClienteByUsuarioID(Number(this.authService.tokenDecoded.nameid))
+    this.getData();
+    this.fillComboCanales();
+    this.getArticulosDePesosExtras();
+  }
+  
+  fillComboCanales(){
+    this.canales.push({codigo:0,nombre:"Todos",grupo:'',grupoID:''})
+    this.canales.push({codigo:1,nombre:"Preventa",grupo:'',grupoID:''})
+    this.canales.push({codigo:2,nombre:"Moderno",grupo:'',grupoID:''})
+    this.canales.push({codigo:3,nombre:"Empleado",grupo:'',grupoID:''})
+    this.canales.push({codigo:4,nombre:"Directo",grupo:'',grupoID:''})
   }
   getData() {
     this.Cargando = true;
 
-    let parametros: Parametro[] = [{ key: "Search", value: this.Search },
-                                    { key: "UsuarioId", value: Number(this.authService.tokenDecoded.nameid) },
-  ]
+    let parametros: Parametro[] = [{ key: "Search", value: this.Search }, ]
 
-    this.httpService.GetAllWithPagination<DespachoPedidoListadoViewModel>(DataApi.PedidosEmpleado,
-       "GetPedidosEmpleadoListado", "ID", this.paginaNumeroActual,
-      this.paginaSize, false, parametros).subscribe(x => {
+    this.httpService.GetAllWithPagination<DespachoPedidoListadoViewModel>(DataApi.Despacho,
+       "GetDespachoListado", "ID", this.paginaNumeroActual,
+      this.paginaSize,true, parametros).subscribe(x => {
 
         if (x.ok) {
           this.data = x.records;
@@ -105,7 +124,7 @@ export class DespachoListadoComponent implements OnInit {
     this.pedidoEmpleadoDetalles = [];
     this.getPedidoEmpleadoDetalle(pedidoEmpleado.id);
     this.pedidoEmpleadoSeleccionado = pedidoEmpleado;
-    this.modalService.open(content, { size: 'lg', });
+    this.modalService.open(content, { windowClass: "myCustomModalClass", backdrop: "static", });
   }
 
   openModalAutorizar(content, pedidoEmpleado: DespachoPedidoListadoViewModel) {
@@ -121,8 +140,8 @@ export class DespachoListadoComponent implements OnInit {
   }
   getPedidoEmpleadoDetalle(pedidoEmpleadoId: number) {
     this.loadingPedidoEmpleadoDetalle = true;
-    this.httpService.DoPostAny<DespachoPedidoDetalleViewModel>(DataApi.PedidosEmpleado,
-      "GetPedidosEmpleadoDetalles", pedidoEmpleadoId).subscribe(response => {
+    this.httpService.DoPostAny<DespachoPedidoDetalleViewModel>(DataApi.Despacho,
+      "GetDespachoDetalles", pedidoEmpleadoId).subscribe(response => {
 
         if (!response.ok) {
           this.toastService.error(response.errores[0]);
@@ -135,32 +154,7 @@ export class DespachoListadoComponent implements OnInit {
         this.toastService.error("No se pudo obtener el detalle", "Error conexion al servidor");
       });
   }
-
-  validExistPedidoSinFacturar() {
-    this.loadingValidaExistPedidoSinFacturar = true;
-    this.httpService.DoPostAny<DespachoPedidoDetalleViewModel>(DataApi.PedidosEmpleado,
-      "GetExistePedidoSinFacturarEmpleado", Number(this.authService.tokenDecoded.nameid)).subscribe(response => {
-
-        if (!response.ok) {
-          this.toastService.error(response.errores[0]);
-        } else {
-          if(response.valores.length>0){
-             if(response.valores[0]>0){
-              this.toastService.warning("No puede crear un nuevo pedido");
-             }else{
-              this.router.navigateByUrl('/ventas/pedidos-empleado/0');
-             }
-          }else{
-            this.router.navigateByUrl('/ventas/pedidos-empleado/0');
-          }
-        }
-        this.loadingValidaExistPedidoSinFacturar = false;
-      }, error => {
-        this.loadingValidaExistPedidoSinFacturar = false;
-        this.toastService.error("No se pudo validar la existencia de pedidos sin facturar", "Error conexion al servidor");
-      });
-  }
-
+ 
 
 
   // CambiarEstadoAutorizacionCotizacion(cotizacionID: number) {
@@ -183,31 +177,10 @@ export class DespachoListadoComponent implements OnInit {
   // }
 
 
-  getEstados() {
-
-    this.loadingEstados = true;
-    this.httpService.DoPost<ComboBox>(DataApi.ComboBox,
-      "GetEstadosCotizacion", null).subscribe(response => {
-
-        if (!response.ok) {
-          this.toastService.error(response.errores[0]);
-        } else {
-          this.estados = response.records;
-        }
-        this.loadingEstados = false;
-      }, error => {
-        this.loadingEstados = false;
-        this.toastService.error("No se pudo obtener los estados", "Error conexion al servidor");
-
-        setTimeout(() => {
-          this.getEstados();
-        }, 1000);
-
-      });
-  }
+ 
 
 
-  cancelarPedidoEmpleado() {
+  cancelarDespacho() {
     this.modalService.dismissAll();
    this.pedidoEmpleadoSeleccionado.loadingCancelPedido=true;
    let pedido={"Id":this.pedidoEmpleadoSeleccionado.id
@@ -215,15 +188,15 @@ export class DespachoListadoComponent implements OnInit {
                ,"ClienteId":this.pedidoEmpleadoSeleccionado.clienteId
               }
               console.log(pedido)
-    this.httpService.DoPostAny<ComboBox>(DataApi.PedidosEmpleado,
-      "CancelarPedidoEmpleado",  pedido).subscribe(response => {
+    this.httpService.DoPostAny<ComboBox>(DataApi.Despacho,
+      "CancelarDespacho",  pedido).subscribe(response => {
 
         if (!response.ok) {
           this.toastService.error(response.errores[0]);
           console.error(response.errores[0]);
         } else {
           this.getData();
-          this.toastService.success("Pedido cancelado", "OK");
+          this.toastService.success("Despacho cancelado", "OK");
         }
         this.pedidoEmpleadoSeleccionado.loadingCancelPedido=false;
 
@@ -262,5 +235,61 @@ export class DespachoListadoComponent implements OnInit {
       });
   }
 
+
+
+  // MOVER A OTRO COMPONENTE
+  getArticulosDePesosExtras() {
+    this.loadingArticulosExtras = true;
+    this.httpService.DoPost<ArticuloPesosExtras>(DataApi.Articulo,
+      "GetArticulosDePesosExtras", null).subscribe(response => {
+
+        if (!response.ok) {
+          this.toastService.error(response.errores[0]);
+        } else {
+          this.articulosExtrasComboBox = response.records;
+
+
+          this.formatArticulosExtras()
+        }
+        this.loadingArticulosExtras = false;
+      }, error => {
+        this.loadingArticulosExtras = false;
+        this.toastService.error("No se pudo obtener los articulos extras", "Error conexion al servidor");
+
+        setTimeout(() => {
+          this.getArticulosDePesosExtras();
+        }, 1000);
+      });
+  }
+
+  formatArticulosExtras() {
+    this.articulosExtras = []
+    this.articulosExtrasComboBox.forEach(a => {
+
+      if (!this.articulosExtras.some(x => x.articuloID == a.articuloID)) {
+        let item: ArticuloPesosExtrasViewModel = new ArticuloPesosExtrasViewModel();
+
+        item.articuloID = a.articuloID
+        item.codigoReferencia = a.codigoReferencia
+        item.nombre = a.nombre
+        item.cantidadSeleccionada = a.cantidadDefault;
+
+
+        item.pesos = this.articulosExtrasComboBox.
+          filter(ar => ar.articuloID == a.articuloID).
+          map(art => {
+            return {
+              "nombre": `${art.valor} ${art.abreviatura}`,
+              "valor": art.valor,
+              "abreviatura": art.abreviatura,
+              "medidaValor": art.medidaValor
+            }
+          });
+
+        this.articulosExtras.push(item)
+      }
+
+    })
+  }
 
 }
