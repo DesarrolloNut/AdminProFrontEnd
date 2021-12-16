68df15454dae3c5f2f90a59c4a2e334391f8d9f1
Software Analyst <jancer.fabian@gmail.com>
2021-12-16 09:52:23 -0400

    http request - retry(3)

--- src/app/core/http/service/backend.service.ts
+++ src/app/core/http/service/backend.service.ts
@@ -5,6 +5,7 @@ import { ResponseContenido } from '../model/ResponseContenido';
 import { Observable } from 'rxjs';
 import { Paginacion } from '../model/Paginacion';
 import { DataApi, dataApiRootMap } from '../../../shared/enums/DataApi.enum';
+import { retry } from 'rxjs/operators';
 
 
 @Injectable({
@@ -29,17 +30,17 @@ export class BackendService {
         request.pagina.paginaSize = PaginaSize;
         request.pagina.ordenAsc = OrderASC;
         request.pagina.ordenColumna = Columna;
-        return this.http.post<ResponseContenido<T>>(this.baseUrl + dataApiRootMap[api] + "/" + Method, request);
+        return this.http.post<ResponseContenido<T>>(this.baseUrl + dataApiRootMap[api] + "/" + Method, request).pipe(retry(3));
     }
 
     public DoPost<T>(api: DataApi, Method: string, parametros: any): Observable<ResponseContenido<T>> {
         let request = new RequestContenido<T>();
         request.parametros = parametros;
-        return this.http.post<ResponseContenido<T>>(this.baseUrl + dataApiRootMap[api] + "/" + Method, request);
+        return this.http.post<ResponseContenido<T>>(this.baseUrl + dataApiRootMap[api] + "/" + Method, request).pipe(retry(3));
     }
 
     public DoPostAny<T>(api: DataApi, Method: string, request: any, reportProgress = false): Observable<ResponseContenido<T>> {
-        return this.http.post<ResponseContenido<T>>(this.baseUrl + dataApiRootMap[api] + "/" + Method, request);
+        return this.http.post<ResponseContenido<T>>(this.baseUrl + dataApiRootMap[api] + "/" + Method, request).pipe(retry(3));
     }
     public async DoPostAnyAsync<T>(api: DataApi, Method: string, request: any, reportProgress = false) {
         return  await this.http.post<ResponseContenido<T>>(this.baseUrl + dataApiRootMap[api] + "/" + Method, request).toPromise();
