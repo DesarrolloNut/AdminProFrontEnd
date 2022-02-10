64da745ffa4c87c0d4c91d89a9214686bc3b7c6e
Software Analyst <jancer.fabian@gmail.com>
2022-02-10 15:29:40 -0400

    FINANZAS | creacion del componente de cheques devueltos

---
+++ src/app/Modules/finanzas/cuentas-por-cobrar/cheques-devueltos/cheques-devueltos-formulario/cheques-devueltos-formulario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cheques-devueltos-formulario',
+  templateUrl: './cheques-devueltos-formulario.component.html',
+  styleUrls: ['./cheques-devueltos-formulario.component.scss']
+})
+export class ChequesDevueltosFormularioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/Modules/finanzas/cuentas-por-cobrar/cheques-devueltos/cheques-devueltos-listado/cheques-devueltos-listado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cheques-devueltos-listado',
+  templateUrl: './cheques-devueltos-listado.component.html',
+  styleUrls: ['./cheques-devueltos-listado.component.scss']
+})
+export class ChequesDevueltosListadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
