9fecec7f23abf467cd1c3cadb2cc4ab24d257288
Juan Luis Velez <jvelez@nutriciosa.com>
2026-03-02 13:07:54 -0400

    feat: Add SCSS cleanup script and production environment configuration, and update package dependencies.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
- //apiUrl: 'https://appadmin.nutriciosa.com/NutriciosaAdminWeb/',
-   ///apiUrl: 'http://192.168.0.174/NutriciosaAdmin/'
-//     apiUrl: 'http://testapp.nutriciosa.com:8888/NutriciosaAdmin/',
+  //apiUrl: 'https://appadmin.nutriciosa.com/NutriciosaAdminWeb/',
+  ///apiUrl: 'http://192.168.0.174/NutriciosaAdmin/'
+  //     apiUrl: 'http://testapp.nutriciosa.com:8888/NutriciosaAdmin/',
 
-apiUrl: 'http://localhost:50551/',
+  apiUrl: 'http://adminpro.nutriciosa.local/',
 };
